459fb255ed140b1471a20f53b0171e88cb6d798a
Frank Buono <fbuonojr@fordham.edu>
2020-09-28 21:27:10 -0400

    added brand statement and picture

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,11 +4,21 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 
 function AboutMe() {
-    return(
+    return (
         <Container>
             <Row>
                 <Col size="lg-12">
                     <h1>About Me</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col size="lg-3">
+
+                </Col>
+                <Col size="lg-9">
+                    <p>
+                        Full stack web developer building on a degree in math and computer/information sciences. Currently earning a certificate in full stack web development from Rutgers University with newly honed skills in HTML, CSS, JavaScript, Node.js. Patience, professionalism, and collaborative disposition were previously put to use as a tutor for high school computer science students and as a key contributor to a research project requiring the combined skills of my college major. Outside of my current field, I worked at a wealth management firm where I perfected the communication and organizational skills which led to the success of a group effort to develop a video game application. I find myself continuously excited by each new aspect of web development, and I have a particular affinity for JavaScript. I am eager to use all of my work experience, technical skills and my steep learning curve as a full stack developer at a large tech company.
+                    </p>
                 </Col>
             </Row>
         </Container>
