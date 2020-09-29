b1ac25668ef32ed76fefd5c80f5c4d74defdd920
Frank Buono <fbuonojr@fordham.edu>
2020-09-28 20:21:53 -0400

    added wrapper to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
         <Header/>
+        <Wrapper>
+          
+        </Wrapper>
       </div>
     </Router>
   );
